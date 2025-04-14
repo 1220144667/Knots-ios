8f21061a7d0caebe5eb7221a84de93bcd8185ccf
洪陪 <hp2022a@163.com>
2025-04-14 10:06:39 +0800

    project init

---
+++ Knots/KnotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnotsApp.swift
+//  Knots
+//
+//  Created by 文伍 on 2025/4/13.
+//
+
+import SwiftUI
+
+@main
+struct KnotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
